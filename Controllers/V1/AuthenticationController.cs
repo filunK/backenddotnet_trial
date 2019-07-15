278c45fc429be65796a7c40883acc5c17faa4bc8
FilunK <yishikawa@koromotani.sakura.ne.jp>
2019-07-15 22:19:22 +0900

    リファクタリング

---
+++ Controllers/V1/AuthenticationController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+
+using FilunK.backenddotnet_trial.Models;
+using FilunK.backenddotnet_trial.Models.Configure;
+using FilunK.backenddotnet_trial.Models.Authentication;
+using FilunK.backenddotnet_trial.Utils;
+
+
+namespace FilunK.backenddotnet_trial.Controllers
+{
+
+    /// <summary>
+    ///     ログインAPI
+    /// </sumamry>
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly AppSettings _config ;
+
+        public AuthenticationController(IOptions<AppSettings> configuration)
+        {
+            this._config = configuration.Value;
+        }
+
+        /// <summary>
+        ///     ログイン
+        /// </summary>
+        /// <remark>
+        ///     このAPIでは以下のJSONを<c>POST</c>メソッドで受け取ります。
+        ///     <code>
+        ///         {
+        ///             username: string
+        ///             password: string
+        ///          }
+        ///     </code>
+        ///     ログインに成功した場合、JWTを返却します。
+        /// </remark>
+        ///
+        [Route("login")]
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] LoginModel model)
+        {
+            IActionResult response = Unauthorized();
+
+            // ログイン認証
+            var user = this.Authenticate(model);
+
+            if (user != null)
+            {
+                // JWT生成
+                var generatedToken = TokenUtil.GenerateToken(this._config.Jwt.Issuer, this._config.Jwt.Issuer, this._config.Jwt.ExpireMinutes, user, this._config.Jwt.TokenKey, SecurityAlgorithms.HmacSha256);
+                var refreshToken = TokenUtil.GenerateRefreshToken(this._config.Jwt.Issuer, this._config.Jwt.Issuer, generatedToken, this._config.Jwt.RefreshTokenKey, SecurityAlgorithms.HmacSha512);
+                response = Ok(new {
+                    token = generatedToken,
+                    refresh = refreshToken
+                });
+            }
+
+            return response;
+        }
+
+        /// <summary>
+        ///     認証有効性確認
+        /// </summary>
+        /// <remark>
+        ///     JWT認証の正常性を確認します。
+        ///     承認できれば200を、再認証が必要である場合は401を返します。
+        /// </remark>
+        ///
+        [Route("confirm")]
+        [Authorize]
+        [HttpGet]
+        public IActionResult Confirm()
+        {
+            return Ok();
+        }
+
+
+
+        #region 非HTTPメソッド
+
+        /// <summary>
+        ///     ユーザ情報を取得する
+        /// </summary>
+        private UserModel Authenticate(LoginModel login)
+        {
+            UserModel user = new UserModel{
+                UserName = login.Username,
+            };
+
+            user.MailAddress = "dummy@dummy.com";
+            user.BirthDate = DateTime.Now;
+
+            return user;
+        }
+
+        #endregion
+    }
+}
---
+++ Utils/TokenUtil.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+
+using FilunK.backenddotnet_trial.Models;
+
+namespace FilunK.backenddotnet_trial.Utils
+{
+
+    /// <summary>
+    ///     トークンユーティリティ
+    /// </summary>
+    public static class TokenUtil
+    {
+
+        /// <summary>
+        ///     JWTを生成する
+        /// </summary>
+        public static string GenerateToken(string issuer, string audience, double expires, UserModel user,string tokenString, string algorithm)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenString));
+            var credential = new SigningCredentials(key, algorithm);
+
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                claims: new[] {
+                    new Claim("username", user.UserName),
+                    new Claim("mailaddress", user.MailAddress),
+                    new Claim("birthday", user.BirthDate.ToLongDateString()),
+                },
+                expires: DateTime.Now.AddMinutes(expires),
+                signingCredentials: credential
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+
+        /// <summary>
+        ///     リフレッシュトークンを生成する
+        /// </summary>
+        public static string GenerateRefreshToken(string issuer, string audience, string jwt, string tokenString, string algorithm)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenString));
+            var credential = new SigningCredentials(key, algorithm);
+
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                claims: new[] {
+                    new Claim("token", jwt),
+                },
+                signingCredentials: credential
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+
+
+}
+
